df19876c4c6785173e2eee7a62b29f7052f12e3a
shoiebjaveed <45892207+shoiebjaveed@users.noreply.github.com>
2022-04-25 06:37:05 +0530

    getting data from crudcrud and showing on the screen

--- main.js
+++ main.js
@@ -17,5 +17,27 @@ function saveData(event){
     }).catch((err) => {
         console.log(err)
     });
+}
+window.addEventListener('DOMContentLoaded', () => {
+    axios.get('https://crudcrud.com/api/f711cf8e782c47b7bbc117d4c06a32e9/userDetails')
+    .then((result) => {
+        showUsers(result.data)
+        console.log(result);
 
+        for (var i = 0; i < result.data.length; i++){
+            showUsers(result.data[i])
+        }
+    }).catch((err) => {
+        console.log(err)
+    });
+})
+
+function showUsers(user){
+    document.getElementById('username').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('phonenumber').value = '';
+
+    const parentNode = document.getElementById('listOfUser');
+    const childHtml =`<li> ${user.name} | ${user.email} | ${user.phone} </li>`
+    parentNode.innerHTML = parentNode.innerHTML+childHtml;
 }
