93298389c38b19c236f00ed618fc9da4dc55faf6
shoiebjaveed <45892207+shoiebjaveed@users.noreply.github.com>
2022-04-25 05:41:18 +0530

    updating user values to crudcrud server

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
-function saveData(){
+function saveData(event){
+    event.preventDefault()
     let name, email, phone;
-    name = document.getElementById("username").value;
-    email = document.getElementById("email").value;
-    phone = document.getElementById("phonenumber").value;
+    name = event.target.username.value;
+    email = event.target.email.value;
+    phone = event.target.phonenumber.value;
     
-    const obj = {
+    let userDetails = {
         name,
         email,
         phone
-    }
-    axios.post("https://crudcrud.com/api/baf1a158f0584e288fa753cbe833cbb1", obj).then((response) => {
-        console.log(response)
+    };
+
+    axios.post('https://crudcrud.com/api/f711cf8e782c47b7bbc117d4c06a32e9/userDetails', userDetails)
+    .then((result) => {
+        console.log(result);
     }).catch((err) => {
         console.log(err)
     });
+
 }
