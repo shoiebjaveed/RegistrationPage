c192aec6c5365c36d1555eb24dcb11fbd478325d
shoiebjaveed <45892207+shoiebjaveed@users.noreply.github.com>
2022-04-27 12:37:36 +0530

    deleting userdetails using network call and from screen

--- main.js
+++ main.js
@@ -11,15 +11,16 @@ function saveData(event){
         phone
     };
 
-    axios.post('https://crudcrud.com/api/f711cf8e782c47b7bbc117d4c06a32e9/userDetails', userDetails)
+    axios.post('https://crudcrud.com/api/b1d9358ef6a448ea90d9b420eb0bd836/userDump', userDetails)
     .then((result) => {
+        showUsers(result.data)
         console.log(result);
     }).catch((err) => {
         console.log(err)
     });
 }
 window.addEventListener('DOMContentLoaded', () => {
-    axios.get('https://crudcrud.com/api/f711cf8e782c47b7bbc117d4c06a32e9/userDetails')
+    axios.get('https://crudcrud.com/api/b1d9358ef6a448ea90d9b420eb0bd836/userDump')
     .then((result) => {
         showUsers(result.data)
         console.log(result);
@@ -38,6 +39,26 @@ function showUsers(user){
     document.getElementById('phonenumber').value = '';
 
     const parentNode = document.getElementById('listOfUser');
-    const childHtml =`<li> ${user.name} | ${user.email} | ${user.phone} </li>`
+    const childHtml =`<li id='${user._id}'> ${user.name} | ${user.email} | ${user.phone} 
+                           <button onclick=deleteData('${user._id}')>Delete</button> 
+                           </li>`
     parentNode.innerHTML = parentNode.innerHTML+childHtml;
+}
+
+function deleteData(userId){
+    axios.delete(`https://crudcrud.com/api/b1d9358ef6a448ea90d9b420eb0bd836/userDump/${userId}`)
+    .then((result) => {
+        deleteFromScreen(userId);
+        console.log(result);
+    }).catch((err) => {
+        console.log(err)
+    });
+}
+
+function deleteFromScreen(userId){
+    const parentNode = document.getElementById('listOfUser');
+    const childNodeToBeDeleted = document.getElementById(userId);
+        if(childNodeToBeDeleted) {
+            parentNode.removeChild(childNodeToBeDeleted);
+            }
 }
